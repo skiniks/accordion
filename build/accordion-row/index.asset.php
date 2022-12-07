b96a6ab62768bdcd3faee546a7af5171032b988b
Ryan Skinner <ryan@officebureau.ca>
2022-12-06 19:26:00 -0500

    adding build

---
+++ build/accordion-row/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c5ff2d770d2d5571feb5');
---
+++ build/accordion/script.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd6e9be56a76a50d14caa');
